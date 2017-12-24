0be35e203b0f8aed92ff4ebeb38c75d828f7d570
Piotr Skowronek <p.skowronek90@gmail.com>
2017-12-24 17:00:48 +0100

    Removed username field

--- tweets.sql
+++ tweets.sql
@@ -1,7 +1,6 @@
 CREATE TABLE Tweets (
 tweet_id INT (11) NOT NULL AUTO_INCREMENT,
 user_id INT (11),
-username VARCHAR (255) NOT NULL,
 text TEXT NOT NULL,
 creationDate DATETIME,
 PRIMARY KEY (tweet_id),

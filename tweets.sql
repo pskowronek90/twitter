143dcd33f988eca88c55f9ef81c33dffbeeb9518
Piotr Skowronek <p.skowronek90@gmail.com>
2017-12-28 18:01:38 +0100

    moved .sql files into dedicated folder

--- tweets.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Tweets (
-tweet_id INT (11) NOT NULL AUTO_INCREMENT,
-user_id INT (11),
-text TEXT NOT NULL,
-creationDate DATETIME,
-PRIMARY KEY (tweet_id),
-FOREIGN KEY (user_id) REFERENCES Users (id)
-);

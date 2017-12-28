17608ffc903ccf589f0cfea6db0527c163f1b40d
Piotr Skowronek <p.skowronek90@gmail.com>
2017-12-28 19:13:26 +0100

    Fix - added missing text column

--- db/comments.sql
+++ db/comments.sql
@@ -2,6 +2,7 @@ CREATE TABLE Comments (
   comm_id INT (11) NOT NULL AUTO_INCREMENT,
   user_id INT (11) NOT NULL,
   post_id INT (11) NOT NULL,
+  text TEXT NOT NULL,
   creationDate DATETIME,
   PRIMARY KEY (comm_id),
   FOREIGN KEY (post_id) REFERENCES Tweets (tweet_id)

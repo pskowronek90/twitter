795822465020604d94e81d20235897ba5e35831e
Piotr Skowronek <p.skowronek90@gmail.com>
2017-12-28 19:04:01 +0100

    Comments file created

--- db/comments.sql
+++ db/comments.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Comments (
   comm_id INT (11) NOT NULL AUTO_INCREMENT,
-  user_id INT (11),
+  user_id INT (11) NOT NULL,
+  post_id INT (11) NOT NULL,
   creationDate DATETIME,
   PRIMARY KEY (comm_id),
-  FOREIGN KEY (user_id) REFERENCES Users (id)
+  FOREIGN KEY (post_id) REFERENCES Tweets (tweet_id)
 );
